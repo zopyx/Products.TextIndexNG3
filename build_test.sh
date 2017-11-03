3c648df031632cff639109bef501a99d21b074b7
Andreas JungÂ <info@zopyx.com>
2017-11-03 17:41:08 +0100

    updated

--- build_test.sh
+++ build_test.sh
@@ -8,25 +8,22 @@ if [[ "$1" = "plone-4.3" ]]
 then
     python_version=2.7
     config=alltests-plone43.cfg
-    buildout=2.5.3
 fi
 
 if [[ "$1" = "plone-5.0" ]]
 then
     python_version=2.7
     config=alltests-plone50.cfg
-    buildout=2.5.3
 fi
 
 if [[ "$1" = "plone-5.1" ]]
 then
     python_version=2.7
     config=alltests-plone51.cfg
-    buildout=2.8.0
 fi
 
-virtualenv-$python_version --clear .
-pip install zc.buildout==$buildout
-buildout bootstrap
-buildout -c $config
+virtualenv-2.7 --clear .
+bin/pip install zc.buildout
+bin/buildout bootstrap
+bin/buildout -c $config
 bin/test
